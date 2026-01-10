c33489e512c5407e1c843f84cb3c0607aea821de
himelsamiul <alamhpl11@gmail.com>
2026-01-11 05:22:04 +0600

    total dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\TourPackage;
+use App\Models\Hotel;
+use App\Models\Transportation;
+use App\Models\Place;
 
 class HomeController extends Controller
 {
     public function dashboard()
     {
-        return view('backend.pages.dashboard');
+        return view('backend.pages.dashboard', [
+            'totalPackages'      => TourPackage::count(),
+            'totalHotels'        => Hotel::count(),
+            'totalTransport'     => Transportation::count(),
+            'totalPlaces'        => Place::count(),
+        ]);
     }
 }
--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -1,85 +1,95 @@
 @extends('backend.master')
+
 @section('content')
 <div class="container-fluid">
 	<h4 class="page-title">Dashboard</h4>
+
 	<div class="row">
+
+		{{-- Total Tour Packages --}}
 		<div class="col-md-3">
-			<div class="card card-stats card-warning">
-				<div class="card-body ">
+			<div class="card card-stats card-primary">
+				<div class="card-body">
 					<div class="row">
 						<div class="col-5">
 							<div class="icon-big text-center">
-								<i class="la la-users"></i>
+								<i class="la la-suitcase"></i>
 							</div>
 						</div>
 						<div class="col-7 d-flex align-items-center">
 							<div class="numbers">
-								<p class="card-category">Visitors</p>
-								<h4 class="card-title">1,294</h4>
+								<p class="card-category">Tour Packages</p>
+								<h4 class="card-title">{{ $totalPackages }}</h4>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+
+		{{-- Total Hotels --}}
 		<div class="col-md-3">
 			<div class="card card-stats card-success">
-				<div class="card-body ">
+				<div class="card-body">
 					<div class="row">
 						<div class="col-5">
 							<div class="icon-big text-center">
-								<i class="la la-bar-chart"></i>
+								<i class="la la-hotel"></i>
 							</div>
 						</div>
 						<div class="col-7 d-flex align-items-center">
 							<div class="numbers">
-								<p class="card-category">Sales</p>
-								<h4 class="card-title">$ 1,345</h4>
+								<p class="card-category">Hotels</p>
+								<h4 class="card-title">{{ $totalHotels }}</h4>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+
+		{{-- Total Transport --}}
+		<div class="col-md-3">
+			<div class="card card-stats card-warning">
+				<div class="card-body">
+					<div class="row">
+						<div class="col-5">
+							<div class="icon-big text-center">
+								<i class="la la-bus"></i>
+							</div>
+						</div>
+						<div class="col-7 d-flex align-items-center">
+							<div class="numbers">
+								<p class="card-category">Transport</p>
+								<h4 class="card-title">{{ $totalTransport }}</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		{{-- Total Places --}}
 		<div class="col-md-3">
 			<div class="card card-stats card-danger">
 				<div class="card-body">
 					<div class="row">
 						<div class="col-5">
 							<div class="icon-big text-center">
-								<i class="la la-newspaper-o"></i>
+								<i class="la la-map-marker"></i>
 							</div>
 						</div>
 						<div class="col-7 d-flex align-items-center">
 							<div class="numbers">
-								<p class="card-category">Subscribers</p>
-								<h4 class="card-title">1303</h4>
+								<p class="card-category">Places</p>
+								<h4 class="card-title">{{ $totalPlaces }}</h4>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-3">
-			<div class="card card-stats card-primary">
-				<div class="card-body ">
-					<div class="row">
-						<div class="col-5">
-							<div class="icon-big text-center">
-								<i class="la la-check-circle"></i>
-							</div>
-						</div>
-						<div class="col-7 d-flex align-items-center">
-							<div class="numbers">
-								<p class="card-category">Order</p>
-								<h4 class="card-title">576</h4>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+
 	</div>
 </div>
-
 @endsection
